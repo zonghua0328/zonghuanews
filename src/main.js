3b4dc39db8d26fc1b37bdb3afb21204af7f1535b
zonghua0328 <2659158308@qq.com>
2021-08-20 11:30:41 +0800

    master

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入和使用vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 配置vant rem布局
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
+// 使用Vant组件库
+Vue.use(Vant)
 
 new Vue({
   router,
